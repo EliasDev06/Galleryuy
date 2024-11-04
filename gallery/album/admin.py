413143e5b5cd729ebf0f5a380457ecd18d5a8fef
kingbaum19 <briantguapo1@gmail.com>
2024-11-04 17:41:39 -0500

    Añadí modelos y migraciones

--- gallery/album/admin.py
+++ gallery/album/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Etiqueta)
+admin.site.register(Albume)
+admin.site.register(Imagen)
--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -1,6 +1,6 @@
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'album'
 ]
 
 MIDDLEWARE = [
@@ -109,6 +110,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = (
+    os.path.join(BASE_DIR, 'media'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
