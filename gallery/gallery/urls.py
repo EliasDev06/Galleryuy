0932b923d2949cfcaaafe00321bbe133e4ee088c
kingbaum19 <briantguapo1@gmail.com>
2024-11-06 18:34:25 -0500

    Incluí la app en urls y corregí un error en settings

--- gallery/gallery/urls.py
+++ gallery/gallery/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import vista
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('album.urls')),
     path('', vista.index, name='index'),
     path('subir_imagenes/', vista.subir_imagenes, name='subir_imagenes'),
     path('validar_cuenta/', vista.validar_cuenta, name='validar_cuenta')
