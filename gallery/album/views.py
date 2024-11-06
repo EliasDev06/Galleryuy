0d46f0c4746844ddbd8167c92dd9a37fbbde7136
kingbaum19 <briantguapo1@gmail.com>
2024-11-06 18:32:57 -0500

    Simplifiqué la base de datos

--- gallery/album/views.py
+++ gallery/album/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Imagen
 
 # Create your views here.
+def index(request):
+    obj = Imagen.objects,all()
+    return render(request, 'index.html',{'image': obj})
